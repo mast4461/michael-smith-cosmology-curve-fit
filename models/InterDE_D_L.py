01d655a3c26fa06778347f4f75d7786a11a0c7bd
mlsmith55 <mlsmith55@gmail.com>
2021-01-01 20:03:01 +0100

    Update InterDE_D_L.py
    Changed the x, which is the independent variable, to the shorthand term EspFact, for expansion factor. The expansion factor is the independent term used in the FLRW model of the Universe expansion. This will allow cosmologists and students quick understanding of the code when made public. The code "connecting the databases to the six models may have to be edited to reflect this change. Also note that the integration limits have been changed to denote 1 as the upper limit, which is the expansion factor of today, all past times have expansion factors between 0 and today (1).

--- models/InterDE_D_L.py
+++ models/InterDE_D_L.py
@@ -12,24 +12,24 @@ from settings import speed_of_light
 """
 The function below is the exact function decribing the FLRW model with the parameters, Hubble constant and matter density. Note this is a two parameter fit and presumes A FLAT UNIVERSE GEOMETRY WITH DARK ENERGY.
 """
-def InterDE_D_L(x, Hubble, Matter):
-    return _portion1(x, Hubble) * _vectorizedIntersum(x, Matter)
+def InterDE_D_L(EspFact, Hubble, Matter):
+    return _portion1(EspFact, Hubble) * _vectorizedIntersum(EspFact, Matter)
 
 """
 _portion1 defines the pre-integral
 """
-def _portion1(x, Hubble):
-    return speed_of_light/(Hubble*x)
+def _portion1(EspFact, Hubble):
+    return speed_of_light/(Hubble*EspFact)
 
 """
 _portion2 is the function to be integrated
 """
-def _portion2(x,Matter):
-    return 1/(x*np.sqrt((Matter/x)+(1-Matter)*x**2))
+def _portion2(EspFact,Matter):
+    return 1/(EspFact*np.sqrt((Matter/EspFact)+(1-Matter)*EspFact**2))
 
 """
 The _intersum is the integration routine 
 """
-def _intersum(x, Matter):
-    return quad(_portion2, x, 1, args=(Matter))[0]
+def _intersum(EspFact, Matter):
+    return quad(_portion2, EspFact, args=(Matter))[0], 1
 _vectorizedIntersum = np.vectorize(_intersum, excluded=["Matter"])
